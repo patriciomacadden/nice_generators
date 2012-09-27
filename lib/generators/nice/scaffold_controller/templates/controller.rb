1d1a119fce630ed237f1c81b9e7793ddbd5a1a74
Patricio Mac Adden <patriciomacadden@gmail.com>
2012-09-27 13:59:11 -0300

    now we use respond_to and respond_with instead of using respond_to in each action

--- lib/generators/nice/scaffold_controller/templates/controller.rb
+++ lib/generators/nice/scaffold_controller/templates/controller.rb
@@ -1,81 +1,53 @@
 <% module_namespacing do -%>
 class <%= controller_class_name %>Controller < ApplicationController
   before_filter :find_<%= singular_table_name %>!
+  respond_to :html, :json
 
   # GET <%= route_url %>
   # GET <%= route_url %>.json
   def index
     @<%= plural_table_name %> = <%= class_name %>.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render <%= key_value :json, "@#{plural_table_name}" %> }
-    end
+    respond_with @<%= plural_table_name %>
   end
 
   # GET <%= route_url %>/1
   # GET <%= route_url %>/1.json
   def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render <%= key_value :json, "@#{singular_table_name}" %> }
-    end
+    respond_with @<%= singular_table_name %>
   end
 
   # GET <%= route_url %>/new
   # GET <%= route_url %>/new.json
   def new
     @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render <%= key_value :json, "@#{singular_table_name}" %> }
-    end
+    respond_with @<%= singular_table_name %>
   end
 
   # GET <%= route_url %>/1/edit
   def edit
+    respond_with @<%= singular_table_name %>
   end
 
   # POST <%= route_url %>
   # POST <%= route_url %>.json
   def create
     @<%= singular_table_name %> = <%= orm_class.build(class_name, "params[:#{singular_table_name}]") %>
-
-    respond_to do |format|
-      if @<%= orm_instance.save %>
-        format.html { redirect_to @<%= singular_table_name %>, <%= key_value :notice, "t(\"controllers.#{plural_table_name}.#{singular_table_name}_was_successfully_created\")" %> }
-        format.json { render <%= key_value :json, "@#{singular_table_name}" %>, <%= key_value :status, ':created' %>, <%= key_value :location, "@#{singular_table_name}" %> }
-      else
-        format.html { render <%= key_value :action, "'new'" %> }
-        format.json { render <%= key_value :json, "@#{orm_instance.errors}" %>, <%= key_value :status, ':unprocessable_entity' %> }
-      end
-    end
+    flash[:notice] = t('controllers.<%= plural_table_name %>.<%= singular_table_name %>_was_successfully_created') if @<%= orm_instance.save %>
+    respond_with @<%= singular_table_name %>
   end
 
   # PUT <%= route_url %>/1
   # PUT <%= route_url %>/1.json
   def update
-    respond_to do |format|
-      if @<%= orm_instance.update_attributes("params[:#{singular_table_name}]") %>
-        format.html { redirect_to @<%= singular_table_name %>, <%= key_value :notice, "t(\"controllers.#{plural_table_name}.#{singular_table_name}_was_successfully_updated\")" %> }
-        format.json { head :no_content }
-      else
-        format.html { render <%= key_value :action, "'edit'" %> }
-        format.json { render <%= key_value :json, "@#{orm_instance.errors}" %>, <%= key_value :status, ':unprocessable_entity' %> }
-      end
-    end
+    flash[:notice] = t('controllers.<%= plural_table_name %>.<%= singular_table_name %>_was_successfully_updated') if @<%= orm_instance.update_attributes("params[:#{singular_table_name}]") %>
+    respond_with @<%= singular_table_name %>
   end
 
   # DELETE <%= route_url %>/1
   # DELETE <%= route_url %>/1.json
   def destroy
     @<%= orm_instance.destroy %>
-
-    respond_to do |format|
-      format.html { redirect_to <%= index_helper %>_url }
-      format.json { head :no_content }
-    end
+    respond_with @<%= singular_table_name %>
   end
 
   private
